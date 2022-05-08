7207c4ad3a8d3a6f62bf1da9a56d320b85ca88aa
Nilo <nilobdrumond@gmail.com>
2022-05-08 15:22:43 -0300

    feat: all urls

--- src/config.ts
+++ src/config.ts
@@ -27,9 +27,12 @@ export const APP_VERSION = '0.0.1';
 // const config = CONFIG[env];
 
 const config = {
-  BASE_URL: 'https://nurse-tasker.herokuapp.com/api/v1/',
+  BASE_URL: 'https://nurse-tasker.herokuapp.com/api/v1',
   AUTH_URL: 'authenticate',
   PRESCRIPTIONS_URL: 'prescricoes',
+  USERS_URL: 'pessoas/usuarios',
+  PATIENTS_URL: 'pacientes',
+  OCCURRENCES: 'ocurrencias',
 };
 
 export default config;
